6191e4fd74cd31ddd3357da1902a71e3d1221296
Silas.K Laptop <Twojstary@gmail.com>
2025-11-30 10:58:29 +0100

    UPDATE
    updating api client url to port 8000

--- frontend/age_recognition/src/services/abstract/ApiClient.js
+++ frontend/age_recognition/src/services/abstract/ApiClient.js
@@ -1,46 +1,42 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 //idk jaki port
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = "http://localhost:8000";
 
 class ApiClient {
-    constructor() {
-        this.client = axios.create({
-            baseURL: API_BASE_URL,
-            timeout: 30000,
-            headers: { 'Content-Type': 'application/json' }
-        });
+  constructor() {
+    this.client = axios.create({
+      baseURL: API_BASE_URL,
+      timeout: 30000,
+      headers: { "Content-Type": "application/json" },
+    });
 
+    this.client.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response && error.response.status === 401) {
+        }
 
-        this.client.interceptors.response.use(
-            response => response,
-            error => {
+        return Promise.reject(error);
+      }
+    );
+  }
 
-                if (error.response && error.response.status === 401) {
+  get(url, config = {}) {
+    return this.client.get(url, config);
+  }
 
-                }
+  post(url, data, config = {}) {
+    return this.client.post(url, data, config);
+  }
 
-                return Promise.reject(error);
-            }
-        );
-    }
+  put(url, data, config = {}) {
+    return this.client.put(url, data, config);
+  }
 
-    get(url, config = {}) {
-        return this.client.get(url, config);
-    }
-
-    post(url, data, config = {}) {
-        return this.client.post(url, data, config);
-    }
-
-    put(url, data, config = {}) {
-        return this.client.put(url, data, config);
-    }
-
-    delete(url, config = {}) {
-        return this.client.delete(url, config);
-    }
+  delete(url, config = {}) {
+    return this.client.delete(url, config);
+  }
 }
 
 export default new ApiClient();
